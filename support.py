7e97e27f0b18f6c97dea8b8e38a8117705ef0bb7
Yaser <y.zerehsaz@gmail.com>
2023-07-13 23:06:12 -0400

    added two functions to support.py and changed requirements

--- support.py
+++ support.py
@@ -1,13 +1,15 @@
 from nltk.corpus import stopwords
 import re
 from nltk.stem import SnowballStemmer
-from typing import List, Dict, Tuple, Sequence
+from typing import List, Dict, Tuple, Sequence, Union
 import logging
 import json
 from functools import reduce
 import os
+from transformers import AutoTokenizer, AutoModel
 
-MAIN_DIRECTORY = "path_to_your_main_dir_followed_by_a_slash"
+# MAIN_DIRECTORY = "path_to_your_main_dir_followed_by_a_slash"
+MAIN_DIRECTORY = "E:/Data_Playground/Analytics/"
 DATA_FILE_NAME = "health_care_titles.txt"
 ADDITIONAL_FREQUENT_WORDS_FILE_NAME = 'additional_frequent_words.json'
 FREQUENT_WORDS_CATEGORIES = ['health', 'general', 'analytics', 'information technology']
@@ -49,29 +51,50 @@ def get_stop_words(add_extra_frequent_words: bool, remove_stop_word_signs: bool,
     return stop_words
 
 
-def find_duplicates(collection_of_titles: Sequence[str]) -> (Dict, int):
+def find_duplicates(collection_of_titles: Sequence[str]) -> Union[Dict, int]:
     """
     param collection_of_titles: a non-empty collection of titles
     return: a dictionary of duplicated titles as keys and their associated indices in the collection as values
     """
-    counter = -1
+    index = -1
     duplicates_count = 0
     collection_of_titles_by_indices = {}
     duplicated_with_indices = {}
     for each_title in collection_of_titles:
-        counter += 1
+        index += 1
         if collection_of_titles_by_indices.get(each_title, 'not_here') != 'not_here':
-            collection_of_titles_by_indices[each_title].append(counter)
+            collection_of_titles_by_indices[each_title].append(index)
             if each_title is not None and each_title != '':
                 duplicates_count += 1
                 try:
-                    duplicated_with_indices[each_title].append(counter)
+                    duplicated_with_indices[each_title].append(index)
                 except KeyError:
                     duplicated_with_indices[each_title] = [collection_of_titles.index(each_title)]
-                    duplicated_with_indices[each_title].append(counter)
+                    duplicated_with_indices[each_title].append(index)
         else:
-            collection_of_titles_by_indices[each_title] = [counter]
+            collection_of_titles_by_indices[each_title] = [index]
     return duplicated_with_indices, duplicates_count
+
+
+def drop_nones_from_sequence(sequence_of_values: Sequence) -> Sequence:
+    number_of_nones = sequence_of_values.count(None)
+    if number_of_nones > 0:
+        sequence_of_values = [each_element for each_element in sequence_of_values if each_element is not None]
+        return sequence_of_values
+    else:
+        logging.warning('There are no Nones in the provided sequence')
+
+
+def find_none_indices(sequence_of_values: Sequence):
+    if not sequence_of_values:
+        raise Exception('Please provide a valid sequence')
+    index = -1
+    none_indices = []
+    for each_element in sequence_of_values:
+        index += 1
+        if each_element is None:
+            none_indices.append(index)
+    return none_indices
 
 
 def process_clean_the_text(remove_stop_words: bool = True,
@@ -146,7 +169,8 @@ def process_clean_the_text(remove_stop_words: bool = True,
         for each_list_of_indices in duplicated_with_indices.values():
             for each_index in each_list_of_indices[1:]:
                 read_lines[each_index] = None
-    read_lines = list(map(lambda each_line: None if each_line == '' and each_line is not None else each_line, read_lines))
+    read_lines = list(
+        map(lambda each_line: None if each_line == '' and each_line is not None else each_line, read_lines))
 
     return {'cleaned_titles': read_lines, 'original_titles': read_lines_original}
 
@@ -205,3 +229,60 @@ def get_frequent_words(path_to_additional_frequent_words: str,
             else:
                 raise Exception(f"Couldn't find the key. Please choose from one of these categories: "
                                 f"{FREQUENT_WORDS_CATEGORIES}")
+
+def transform_word_to_vec(sequence_of_sentences: Sequence[str], tokenizer_pattern: str = TOKEN_PATTERN,
+                          method: str = 'basic', normalize_output: bool = True) -> Sequence[Sequence[float]]:
+    """
+    sequence_of_sentences: the collection of sentences to transform to vectors. Each element must be a non-empty
+    string. None, '' and null values are not acceptable.
+    tokenizer_pattern: a regex for tokenizing the text, this is required if method is set to 'basic'
+    normalize_output: whether to normalize the transformed data
+    method: type of words' transformation:
+        1. basic: this is the simple word-count vectorizer which tries to find lexicographical similarity between
+        phrases.
+        2. embedding_all_mpnet: the 'all_mpnet_base_v2' sentence transformer is used to convert
+        the sentences to vectors. This method can be potentially used for sentiment search.
+    output: a collection with len(list_of_sentences) elements. Each element is a collection of vectorized words
+    associated with a sentence.
+    """
+    if any(map(lambda each_sentence: each_sentence is None, sequence_of_sentences)):
+        raise Exception('The sequence_of_sentences argument must contain valid sentences not Nones')
+
+    if method == 'basic':
+        word_vectorizer = CountVectorizer(token_pattern=tokenizer_pattern)
+        transformed_vectors = word_vectorizer.fit_transform(sequence_of_sentences)
+        if normalize_output:
+            return normalize(transformed_vectors).toarray().tolist()
+        else:
+            return transformed_vectors.toarray().tolist()
+
+    elif method == 'embedding_all_mpnet':
+        transformed_vectors = huggingface_sentence_transform(sequence_of_sentences, normalize_output)
+        return transformed_vectors.tolist()
+    else:
+        raise Exception('Please select either the basic or embedding_all_mpnet method')
+
+
+def huggingface_sentence_transform(sequence_of_sentences: Sequence[str], normalize: bool = False):
+    tokenizer = \
+        AutoTokenizer. \
+            from_pretrained(
+            os.path.join(SENTENCE_TRANSFORMERS_MODELS_FOLDER_HUGGINGFACE, PRETRAINED_EMBEDDING_MODEL_NAME))
+    sentence_transformer_model = \
+        AutoModel. \
+            from_pretrained(
+            os.path.join(SENTENCE_TRANSFORMERS_MODELS_FOLDER_HUGGINGFACE, PRETRAINED_EMBEDDING_MODEL_NAME))
+    tensor_of_indexed_tokens = tokenizer(sequence_of_sentences,
+                                         padding=True,
+                                         truncation=True, return_tensors='pt')
+    with torch.no_grad():
+        token_and_pooled_embeddings = sentence_transformer_model(**tensor_of_indexed_tokens)
+    token_embeddings = token_and_pooled_embeddings[HUGGINGFACE_EMBEDDINGS_OUTPUT_NAME]
+    attention_mask = tensor_of_indexed_tokens['attention_mask']
+    attention_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    pooled_sentence_transforms = torch.sum(token_embeddings * attention_mask_expanded, 1) / torch.clamp(
+        attention_mask_expanded.sum(1), min=1e-9)
+    if normalize:
+        return torch.nn.functional.normalize(pooled_sentence_transforms, p=2, dim=1)
+    else:
+        return pooled_sentence_transforms
