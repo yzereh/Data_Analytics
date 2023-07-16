605053cc95a8d6a88d2753532a7433bb074f6d59
Yaser <y.zerehsaz@gmail.com>
2023-07-16 14:40:47 -0400

    created the class ClusterTitles

--- support.py
+++ support.py
@@ -12,6 +12,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import normalize
 
 MAIN_DIRECTORY = "path_to_your_main_dir_followed_by_a_slash"
+MAIN_DIRECTORY = "E:/Data_Playground/Analytics/"
 DATA_FILE_NAME = "health_care_titles.txt"
 ADDITIONAL_FREQUENT_WORDS_FILE_NAME = 'additional_frequent_words.json'
 FREQUENT_WORDS_CATEGORIES = ['health', 'general', 'analytics', 'information technology']
@@ -20,6 +21,8 @@ TOKEN_PATTERN = r"\b[^\d\W]+\b"
 PRETRAINED_EMBEDDING_MODEL_NAME = 'all-mpnet-base-v2'
 HUGGINGFACE_EMBEDDINGS_OUTPUT_NAME = 'last_hidden_state'
 SENTENCE_TRANSFORMERS_MODELS_FOLDER_HUGGINGFACE = 'sentence-transformers/'
+CLEANED_TITLES_NAME = 'cleaned_titles'
+ORIGINAL_TITLES_NAME = 'original_titles'
 
 
 def get_extra_frequent_words_path() -> str:
@@ -30,7 +33,7 @@ def get_fasttext_lang_detect_path() -> str:
     return os.path.join(MAIN_DIRECTORY, FASTTEXT_LANGUAGE_DETECT_NAME)
 
 
-def is_the_language_english(documents: Sequence[str]) -> List[str]:
+def is_the_language_english(documents: Union[str, Sequence[str], map]) -> List[str]:
     try:
         import fasttext
         path_to_lang_model = get_fasttext_lang_detect_path()
@@ -57,7 +60,7 @@ def get_stop_words(add_extra_frequent_words: bool, remove_stop_word_signs: bool,
     return stop_words
 
 
-def find_duplicates(collection_of_titles: Sequence[str]) -> tuple[dict[str, list[int]], int]:
+def find_duplicates(collection_of_titles: Union[Sequence[str], map]) -> tuple[dict[str, list[int]], int]:
     """
     param collection_of_titles: a non-empty collection of titles
     return: a dictionary of duplicated titles as keys and their associated indices in the collection as values
@@ -103,84 +106,6 @@ def find_none_indices(sequence_of_values: Sequence):
     return none_indices
 
 
-def process_clean_the_text(remove_stop_words: bool = True,
-                           remove_stop_word_signs: bool = True, add_extra_frequent_words: bool = True,
-                           extra_frequent_words_categories: str or Sequence[str] = 'all',
-                           stem_the_words: bool = True, drop_non_english=True) -> Dict:
-    """
-    Parameters:
-                remove_stop_word_signs: whether the signs must be removed from the stopwords or not (True/False).
-                add_extra_frequent_words: whether extra frequently used words must be added to the stopwords
-                (True/False).
-                extra_frequent_words_categories: a single category or a collection of categories to select the
-                most frequently used words from. Default value is 'all' meaning all categories.
-                remove_stop_words: whether stopwords must be removed or not.
-                stem_the_words: whether stemming must be done.
-                drop_non_english: drop if a title is not english.
-    return: a dictionary with two keys, 1. cleand_title: the cleaned data, and
-    2. original_titles: the initial untouched data
-
-    """
-
-    with open(os.path.join(MAIN_DIRECTORY, DATA_FILE_NAME), encoding='UTF-8') as titles:
-        read_lines_original = titles.readlines()
-
-    read_lines = read_lines_original.copy()
-    read_lines = map(lambda each_line: each_line.strip(), read_lines)
-
-    if drop_non_english:
-        read_lines = list(is_the_language_english(read_lines))
-
-    number_of_nones = read_lines.count(None)
-    if number_of_nones > 0:
-        logging.warning(f'The provided data contain {number_of_nones} '
-                        f'non-english titles which will be replaced by Nones.')
-
-    read_lines = list(map(lambda each_line: each_line.upper() if each_line else None, read_lines))
-    read_lines = map(lambda each_line: re.sub(r"[^\w\s]", "", each_line) if each_line else None, read_lines)
-    read_lines = \
-        map(lambda each_line: " ".join(re.findall("[A-Za-z]+", each_line)) if each_line else None, read_lines)
-
-    if stem_the_words:
-        stemmer = SnowballStemmer("english")
-        read_lines = map(lambda each_line: " ".join(stemmer.stem(each_word).upper()
-                                                    for each_word in each_line.split()) if each_line else None,
-                         read_lines)
-
-    if remove_stop_words:
-        stop_words = get_stop_words(add_extra_frequent_words, remove_stop_word_signs,
-                                    extra_frequent_words_categories, stem_the_words)
-        read_lines = map(lambda each_line: " ".join(each_word for each_word in each_line.split()
-                                                    if each_word
-                                                    not in stop_words) if each_line else None, read_lines)
-    read_lines = list(read_lines)
-
-    if read_lines.count('') == 1:
-        logging.warning('After removing the stopwords, one of the titles is removed completely.\n'
-                        'This is probably due to the addition of the extra frequent words to the stopwords.\n'
-                        'The additional_frequent_words.json file can be edited in case you think some information'
-                        'is lost.')
-
-    if read_lines.count('') > 1:
-        logging.warning(f"After removing the stopwords, {read_lines.count('')} of the titles are removed completely.\n"
-                        f"This is probably due to the addition of the extra frequent words to the stopwords.\n"
-                        f"The additional_frequent_words.json file can be edited in case you think some information "
-                        f"is lost.")
-
-    duplicated_with_indices, duplicates_count = find_duplicates(read_lines)
-    if duplicates_count > 0:
-        logging.warning(f'There are {duplicates_count} duplicates in the titles. '
-                        f'All duplicates will be replaced by None.')
-
-        for each_list_of_indices in duplicated_with_indices.values():
-            for each_index in each_list_of_indices[1:]:
-                read_lines[each_index] = None
-    read_lines = list(
-        map(lambda each_line: None if each_line == '' and each_line is not None else each_line, read_lines))
-
-    return {'cleaned_titles': read_lines, 'original_titles': read_lines_original}
-
-
 def download_load_stop_words(remove_signs: bool = True, extra_frequent_words: List[str] = None) -> List:
     """
     param remove_signs: whether to remove signs from stop words or not
@@ -214,14 +139,14 @@ def json_load(path: str) -> Dict:
 
 
 def get_frequent_words(path_to_additional_frequent_words: str,
-                       categories: str or List[str] or Tuple[str] = 'all') -> List:
+                       categories: Union[str, Sequence[str]] = 'all') -> List:
     frequent_words_by_category = json_load(path_to_additional_frequent_words)
     list_frequent_words = []
     if categories == 'all':
         all_frequent_words = reduce(lambda x, y: x + y, frequent_words_by_category.values())
         return all_frequent_words
     else:
-        if isinstance(categories, List) | isinstance(categories, Tuple):
+        if isinstance(categories, Sequence):
             for each_category in categories:
                 if each_category in frequent_words_by_category.keys():
                     list_frequent_words.extend(frequent_words_by_category.get(each_category))
@@ -237,7 +162,7 @@ def get_frequent_words(path_to_additional_frequent_words: str,
                                 f"{FREQUENT_WORDS_CATEGORIES}")
 
 
-def transform_word_to_vec(sequence_of_sentences: Sequence[str], tokenizer_pattern: str = TOKEN_PATTERN,
+def transform_word_to_vec(sequence_of_sentences: Union[Sequence[str], map], tokenizer_pattern: str = TOKEN_PATTERN,
                           method: str = 'basic', normalize_output: bool = True) -> Sequence[Sequence[float]]:
     """
     sequence_of_sentences: the collection of sentences to transform to vectors. Each element must be a non-empty
@@ -270,14 +195,12 @@ def transform_word_to_vec(sequence_of_sentences: Sequence[str], tokenizer_patter
         raise Exception('Please select either the basic or embedding_all_mpnet method')
 
 
-def huggingface_sentence_transform(sequence_of_sentences: Sequence[str], normalize: bool = False):
+def huggingface_sentence_transform(sequence_of_sentences: Union[Sequence[str], str], normalize: bool = False):
     tokenizer = \
-        AutoTokenizer. \
-            from_pretrained(
+        AutoTokenizer.from_pretrained(
             os.path.join(SENTENCE_TRANSFORMERS_MODELS_FOLDER_HUGGINGFACE, PRETRAINED_EMBEDDING_MODEL_NAME))
     sentence_transformer_model = \
-        AutoModel. \
-            from_pretrained(
+        AutoModel.from_pretrained(
             os.path.join(SENTENCE_TRANSFORMERS_MODELS_FOLDER_HUGGINGFACE, PRETRAINED_EMBEDDING_MODEL_NAME))
     tensor_of_indexed_tokens = tokenizer(sequence_of_sentences,
                                          padding=True,
@@ -295,5 +218,7 @@ def huggingface_sentence_transform(sequence_of_sentences: Sequence[str], normali
         return pooled_sentence_transforms
 
 
-
-
+def load_data():
+    with open(os.path.join(MAIN_DIRECTORY, DATA_FILE_NAME), encoding='UTF-8') as titles:
+        read_lines_original = titles.readlines()
+    return read_lines_original
